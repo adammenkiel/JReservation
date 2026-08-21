261ed74fb0c4ff741fedfacf734cf73c66da35e9
adammenkiel <publicprojectsmenkiel@gmail.com>
2026-08-21 12:35:14 +0200

    Initial upload of backend

---
+++ JReservation-Backend/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "JReservation-Backend"
---
+++ JReservation-Backend/src/main/java/pl/publicprojects/jreservation/JReservation.java
@@ -0,0 +1,7 @@
+package pl.publicprojects.jreservation;
+
+public class JReservation {
+    public static void main(String[] args) {
+
+    }
+}
