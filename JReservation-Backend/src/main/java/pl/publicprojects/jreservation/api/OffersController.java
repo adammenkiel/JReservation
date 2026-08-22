95b6b09c528eec9e2e63aaefc209246eb05fabc2
adammenkiel <publicprojectsmenkiel@gmail.com>
2026-08-23 01:33:07 +0200

    Products list support

--- JReservation-Backend/src/main/java/pl/publicprojects/jreservation/api/OffersController.java
+++ JReservation-Backend/src/main/java/pl/publicprojects/jreservation/api/OffersController.java
@@ -1,17 +1,29 @@
 package pl.publicprojects.jreservation.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.publicprojects.jreservation.domain.reservation.ProductInfo;
+import pl.publicprojects.jreservation.infrastructure.repositories.ProductRepository;
 
 @RestController
 @RequestMapping("/app")
 public class OffersController {
 
-    @GetMapping("/offers")
-    public ResponseEntity<?> offers() {
-        return ResponseEntity.ok("Test");
+    private final ProductRepository productRepository;
+
+    public OffersController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping("/offers/{page}")
+    public ResponseEntity<?> offers(int page) {
+        return ResponseEntity.ok(
+                productRepository.getProductsPage(PageRequest.of(page, 10))
+        );
     }
 }
--- JReservation-Backend/src/main/java/pl/publicprojects/jreservation/infrastructure/repositories/ProductRepository.java
+++ JReservation-Backend/src/main/java/pl/publicprojects/jreservation/infrastructure/repositories/ProductRepository.java
@@ -1,13 +1,18 @@
 package pl.publicprojects.jreservation.infrastructure.repositories;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.publicprojects.jreservation.domain.reservation.ProductInfo;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductInfo, UUID> {
     Optional<ProductInfo> getProductByProductId(UUID id);
+    @Query("SELECT product FROM ProductInfo product")
+    List<ProductInfo> getProductsPage(Pageable pageable);
 }
