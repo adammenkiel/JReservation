f6284da42aac8715ea7722e02bcb0e64250ab4ce
adammenkiel <publicprojectsmenkiel@gmail.com>
2026-09-03 21:16:48 +0200

    Add AuthController test

--- JReservation-Backend/src/test/java/pl/publicprojects/jreservation/tests/infrastructure/AuthControllerTests.java
+++ JReservation-Backend/src/test/java/pl/publicprojects/jreservation/tests/infrastructure/AuthControllerTests.java
@@ -1,35 +1,46 @@
 package pl.publicprojects.jreservation.tests.infrastructure;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.HashMap;
+import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-public class OffersControllerTests {
-
+public class AuthControllerTests {
     @LocalServerPort
     private int port;
 
     @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
     private TestRestTemplate restTemplate;
 
+
+    //TODO: Test isn't good because it's important to correct Arrange section
     @Test
-    public void accessWithoutLoginTest() throws Exception {
+    public void loginTest() {
+        //Arrange
+        var bodyMap = new HashMap<>();
+        bodyMap.put("username", "adammenkiel");
+        bodyMap.put("password", "haslo");
+
         //Act
-        ResponseEntity<String> response = this.restTemplate
-                .getForEntity("http://localhost:" + port + "/app/offers/1", String.class);
+        List<String> responseCookies = this.restTemplate.postForEntity(
+            "http://localhost:" + port + "/auth/login",
+            bodyMap,
+            String.class
+        ).getHeaders().get("set-cookie");
+        
         //Assert
-        assertEquals(403, response.getStatusCode().value());
+        Assertions.assertNotNull(responseCookies);
+        Assertions.assertFalse(responseCookies.isEmpty());
     }
 }
